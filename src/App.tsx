e68c599ba8f2f0b77cd7babd43acf55962842b40
hannah_collins <hec@udel.com>
2026-09-02 12:57:20 -0400

    hopfully fix things

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,11 @@ import { Button } from "react-bootstrap";
 function App(): React.JSX.Element {
     return (
         <div className="App">
-            <header className="App-header">
+            <h1 className="App-header">
                 Hannah Collins UD CISC275 with React Hooks and TypeScript
-            </header>
-            <header className="App-header">New Second Header :3</header>
-            <img src="../../Downloads/picture.jpeg" alt="Picture of me" />
+            </h1>
+            <h2 className="App-header">New Second Header :3</h2>
+            <img src="././Downloads/picture.jpg" alt="Picture of me" />
             List of some colors:
             <ul>
                 <li>Yellow</li>
