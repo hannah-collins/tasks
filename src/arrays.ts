7b4dc14e7f0aa8e708942fccb236567dc7c44697
hannah_collins <hec@udel.com>
2026-09-03 13:24:29 -0400

    make math

--- src/arrays.ts
+++ src/arrays.ts
@@ -91,7 +91,14 @@ export function allRGB(colors: string[]): boolean {
  * And the array [] would become "0=0".
  */
 export function makeMath(addends: number[]): string {
-    return "";
+    if (addends.length === 0) {
+        return "0=0";
+    }
+    const sum = addends.reduce(
+        (currentTotal: number, addend: number) => currentTotal + addend,
+        0,
+    );
+    return `${sum}=${addends.join("+")}`;
 }
 
 /**
