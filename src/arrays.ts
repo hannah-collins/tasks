c8287ce03a4eff148b90a4dfcdfaa227d1ec540d
hannah_collins <hec@udel.com>
2026-09-03 13:09:24 -0400

    allRGB

--- src/arrays.ts
+++ src/arrays.ts
@@ -73,7 +73,11 @@ export function countShortWords(words: string[]): number {
  * then return true.
  */
 export function allRGB(colors: string[]): boolean {
-    return false;
+    const checkColors = colors.filter(
+        (color: string): boolean =>
+            color === "red" || color === "blue" || color === "green",
+    );
+    return checkColors.length === colors.length;
 }
 
 /**
