c204f5eed398bd640423f732a7e57f52af2d67eb
hannah_collins <hec@udel.com>
2026-09-12 15:21:41 -0400

    toCSV

--- src/nested.ts
+++ src/nested.ts
@@ -75,7 +75,7 @@ export function sumPoints(questions: Question[]): number {
  * Consumes an array of questions and returns the sum total of the PUBLISHED questions.
  */
 export function sumPublishedPoints(questions: Question[]): number {
-    return 0;
+    return sumPoints(getPublishedQuestions(questions));
 }
 
 /***
@@ -96,7 +96,11 @@ id,name,options,points,published
  * Check the unit tests for more examples!
  */
 export function toCSV(questions: Question[]): string {
-    return "";
+    const questionsStrings = questions.map(
+        (question: Question): string =>
+            `${question.id},${question.name},${question.options.length},${question.points},${question.published}`,
+    );
+    return "id,name,options,points,published\n" + questionsStrings.join("\n");
 }
 
 /**
