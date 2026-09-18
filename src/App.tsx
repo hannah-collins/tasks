8bece06b0ebe3f33f8e68c3a68f17bc1348336cd
hannah_collins <hec@udel.com>
2026-09-18 16:52:41 -0400

    double half

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,9 @@ import { StartAttempt } from "./components/StartAttempt";
 import { TwoDice } from "./components/TwoDice";
 import { CycleHoliday } from "./components/CycleHoliday";
 import { Counter } from "./components/Counter";
-// import { DoubleHalf } from "./bad-components/DoubleHalf";
-import { ColoredBox } from "./bad-components/ColoredBox";
-import { ShoveBox } from "./bad-components/ShoveBox";
+import { DoubleHalf } from "./bad-components/DoubleHalf";
+// import { ColoredBox } from "./bad-components/ColoredBox";
+// import { ShoveBox } from "./bad-components/ShoveBox";
 import { ChooseTeam } from "./bad-components/ChooseTeam";
 
 function App(): React.JSX.Element {
@@ -18,13 +18,13 @@ function App(): React.JSX.Element {
                 New Header :3
             </header>
             <hr></hr>
-            {/* <DoubleHalf></DoubleHalf> */}
+            <DoubleHalf></DoubleHalf>
             <hr></hr>
             <ChooseTeam></ChooseTeam>
             <hr></hr>
-            <ColoredBox></ColoredBox>
+            {/* <ColoredBox></ColoredBox> */}
             <hr></hr>
-            <ShoveBox></ShoveBox>
+            {/* <ShoveBox></ShoveBox> */}
             <hr></hr>
             <Counter></Counter>
             <hr />
