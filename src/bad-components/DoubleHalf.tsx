c045c74acb912486788fb64b916e910702607250
hannah_collins <hec@udel.com>
2026-09-18 17:25:38 -0400

    fix choose team

--- src/bad-components/DoubleHalf.tsx
+++ src/bad-components/DoubleHalf.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
-// import { dhValue, setDhValue } from "./DoubleHalfState";
+
 export function DoubleHalf(): React.JSX.Element {
     const [dhValue, setDhValue] = useState<number>(10);
 
